dbcf4c620638e5666c07a53fe6b11abae3b88957
ricky <rickylu@nexaiot.com>
2022-11-18 11:34:48 +0800

    update README & example

--- examples/main.go
+++ examples/main.go
@@ -18,39 +18,54 @@ func main() {
 		}
 	}()
 
-	q.AddJob(func(ctx context.Context) error {
+	err := q.AddJob(func(ctx context.Context) error {
 		log.Print("job 001")
 		time.Sleep(3 * time.Second)
 		log.Print("job 001 finish")
 		return nil
 	})
+	if err != nil {
+		log.Printf("add job 001 error: %s\n", err.Error())
+	}
 
-	q.AddJob(func(ctx context.Context) error {
+	err = q.AddJob(func(ctx context.Context) error {
 		log.Print("job 002")
 		time.Sleep(5 * time.Second)
 		log.Print("job 002 finish")
 		return nil
 	})
+	if err != nil {
+		log.Printf("add job 002 error: %s\n", err.Error())
+	}
 
-	q.AddJob(func(ctx context.Context) error {
+	err = q.AddJob(func(ctx context.Context) error {
 		log.Print("job 003 default retry")
 		return errors.New("job 003 finish")
 	}, queue.MaxRetries(1), queue.WithBackoff(queue.BackoffLinear(0*time.Second)))
+	if err != nil {
+		log.Printf("add job 003 error: %s\n", err.Error())
+	}
 
 	time.Sleep(1 * time.Second)
 
-	q.AddJob(func(ctx context.Context) error {
+	err = q.AddJob(func(ctx context.Context) error {
 		log.Print("job 004")
 		time.Sleep(1 * time.Second)
 		log.Print("job 004 finish")
 		return nil
 	})
+	if err != nil {
+		log.Printf("add job 004 error: %s\n", err.Error())
+	}
 
-	q.AddJob(func(ctx context.Context) error {
+	err = q.AddJob(func(ctx context.Context) error {
 		log.Print("job 005 custom retry")
 		time.Sleep(1 * time.Second)
 		return errors.New("job 005 finish")
 	}, queue.MaxRetries(3), queue.WithBackoff(queue.BackoffExponential(1*time.Second)))
+	if err != nil {
+		log.Printf("add job 005 error: %s\n", err.Error())
+	}
 
 	<-q.Done()
 }
